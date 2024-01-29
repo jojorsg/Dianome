d53f9c35b3e5a9e124d725ec305c63ce53dae528
jojorsg <josueroberto099@gmail.com>
2024-01-29 15:49:21 -0300

    Initial commit: criando repo Dianome

---
+++ src/test/java/br/com/dianome/entregadores/EntregadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dianome.entregadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
