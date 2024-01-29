ac4e4dbc21021e7416282be48d61b95aa385b912
jojorsg <josueroberto099@gmail.com>
2024-01-29 16:37:26 -0300

    Add pacote model

---
+++ src/main/java/br/com/dianome/entregadores/model/Entregador.java
@@ -0,0 +1,39 @@
+package br.com.dianome.entregadores.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+@Entity
+@Table(name = "entregadores")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Entregador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(unique = true)
+    @Size(min = 11, max = 11, message = "O CPF deve possuir 11 dígitos.")
+    private BigInteger cpf;
+
+    @NotBlank
+    @Size(max = 100)
+    private String nome;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Tipo veiculo;
+}
---
+++ src/main/java/br/com/dianome/entregadores/model/Tipo.java
@@ -0,0 +1,8 @@
+package br.com.dianome.entregadores.model;
+
+public enum Tipo {
+
+    LEVE,
+    MEDIO,
+    PESADO
+}
