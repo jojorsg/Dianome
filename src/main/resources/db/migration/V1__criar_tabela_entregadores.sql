dd27ae762b184d8637ddce7241941b57f680038e
jojorsg <josueroberto099@gmail.com>
2024-01-30 17:54:38 -0300

    Subindo migration V1

---
+++ src/main/resources/db/migration/V1__criar_tabela_entregadores.sql
@@ -0,0 +1,10 @@
+CREATE TABLE entregadores (
+ id bigint(20) NOT NULL AUTO_INCREMENT,
+ cpf bigint(11) NOT NULL,
+ nome varchar(100) DEFAULT NULL,
+ tipo varchar(255) NOT NULL,
+ PRIMARY KEY (id)
+)
+
+ALTER TABLE entregador
+ ADD CONSTRAINT cpf_unico UNIQUE (cpf);
